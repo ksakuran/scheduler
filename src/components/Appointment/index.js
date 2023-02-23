076fddef6244b5240ed95299adc0119fcd63ec8d
Katherine Nishimura <katherine.nishimura@live.ca>
2023-02-22 18:11:19 -0800

    add story for empty app, and booked apt, add header, show and empty to appointment index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,7 +10,7 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview && <Show student={props.student} interviewer={props.interviewer}/>}
+      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
       {!props.interview && <Empty/>}
     </article>
   );
