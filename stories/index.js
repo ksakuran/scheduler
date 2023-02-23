1398abdd2bda830bddec056da31ff13ce94fa96d
Katherine Nishimura <katherine.nishimura@live.ca>
2023-02-22 16:13:36 -0800

    add show component and stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -146,3 +147,4 @@ storiesOf("InterviewerList", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} student={"Lydia Miller-Jones"} interviewer={interviewers[0]}/>)
