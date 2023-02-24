38c22071627b3f0c121ee05cc5fed3f365f1fae0
Katherine Nishimura <katherine.nishimura@live.ca>
2023-02-23 18:26:28 -0800

    add proxy for axios request, add useEffect axios req for days in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
@@ -46,29 +47,18 @@ const appointments = {
 };
 
 
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  const [day, setDay] = useState("Monday")
+
+  useEffect(() => {
+    Axios.get('/api/days')
+      .then(response => {
+        console.log("useEffect Days response.data:",response.data)
+        setDays([...response.data])
+    })
+  }, [])
 
   const appointmentListItems = Object.values(appointments).map(
     (appointment) => {
