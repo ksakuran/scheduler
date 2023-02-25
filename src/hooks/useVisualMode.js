01f0e27d3b07d8d1b635cc9e39841b92cc64c0f0
Katherine Nishimura <katherine.nishimura@live.ca>
2023-02-24 18:22:32 -0800

    add custom hook use visual mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+  
+  
+  function transition(secondary, replace = false) {
+
+    if(replace) {
+      history.pop()
+      setMode(secondary)
+    }
+
+    setHistory(prev => {
+      return [...prev, secondary];
+    })
+    setMode(secondary)
+  }
+  
+  function back() {
+    if (history.length > 1){
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+    if (history.length === 1) {
+      setMode(initial);
+    }
+  }
+  return { mode, transition, back };
+}
