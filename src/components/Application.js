fcc06e8812a7b54c8a0f936ce781c8bb31ec851c
Katherine Nishimura <katherine.nishimura@live.ca>
2023-02-24 16:54:56 -0800

    add helper selector to get interview information

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,70 +4,21 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
-
-
-
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
 
-  // const [days, setDays] = useState([]);
-  // const [day, setDay] = useState("Monday")
-
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day)
-
-  //setState({ ...state, day: "Monday" });
 
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState(prev => ({ ...prev, days }));
-
 
   // sets days, empty array for dependency since days only needs to be set once
   useEffect(() => {
@@ -76,51 +27,59 @@ export default function Application(props) {
       Axios.get('/api/appointments'),
       Axios.get('/api/interviewers')
     ]).then((res) => {
-      console.log("0",res[0].data);
-      console.log("1",res[1].data);
-      console.log("2",res[2].data);
-      setState(prev => ({...prev, days: res[0].data, appointments: res[1].data}))
-    })
-}, [])
+      console.log("0", res[0].data);
+      console.log("1", res[1].data);
+      console.log("2", res[2].data);
+      setState(prev => ({ ...prev, days: res[0].data, appointments: res[1].data, interviewers: res[2].data }));
+    });
+  }, []);
 
-const appointmentListItems = Object.values(dailyAppointments).map(
-  (appointment) => {
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+
+
+  const appointmentListItems = Object.values(dailyAppointments).map(
+    (appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
-  }
-);
+  });
 
-appointmentListItems.push(<Appointment key="last" time="5pm" />);
+  appointmentListItems.push(<Appointment key="last" time="5pm" />);
 
-return (
-  <main className="layout">
-    <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu">
-        <DayList
-          days={state.days}
-          value={state.day}
-          setDay={setDay}
+  return (
+    <main className="layout">
+      <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
         />
-      </nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
-    </section>
-    <section className="schedule">
-      {appointmentListItems}
-    </section>
-  </main>
-);
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            value={state.day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+      </section>
+      <section className="schedule">
+        {appointmentListItems}
+      </section>
+    </main>
+  );
 }
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -21,3 +21,36 @@ export function getAppointmentsForDay(state, dayOfWeek) {
 
   return appointmentInfo;
 };
+
+/*The function should return a new object containing the interview data when we pass it an object that contains the interviewer. Otherwise, the function should return null. The object it returns should look like this:
+
+{  
+  "student": "Lydia Miller-Jones",
+  "interviewer": {  
+    "id": 1,
+    "name": "Sylvia Palmer",
+    "avatar": "https://i.imgur.com/LpaY82x.png"
+  }
+} 
+state.appointments["2"].interview
+*/
+
+export function getInterview(state, appointmentIdInterview) {
+  
+  if (appointmentIdInterview === null) {
+    return null;
+  }
+
+  const interviewerId = appointmentIdInterview.interviewer
+
+  const interview = {  
+    "student": appointmentIdInterview.student,
+    "interviewer": {  
+      "id": interviewerId,
+      "name": state.interviewers[interviewerId].name,
+      "avatar": state.interviewers[interviewerId].avatar
+    }
+  }
+
+  return interview;
+}
